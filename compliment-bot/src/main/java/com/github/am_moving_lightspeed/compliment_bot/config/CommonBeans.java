16e7469462e6fc64940d58fb5724a9b22c4596c5
am-moving-lightspeed <am.moving.lightspeed@gmail.com>
2024-06-21 20:07:05 +0300

    Added basic persistence

--- compliment-bot/src/main/java/com/github/am_moving_lightspeed/compliment_bot/config/CommonBeans.java
+++ compliment-bot/src/main/java/com/github/am_moving_lightspeed/compliment_bot/config/CommonBeans.java
@@ -1,8 +1,13 @@
 package com.github.am_moving_lightspeed.compliment_bot.config;
 
+import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.am_moving_lightspeed.compliment_bot.domain.service.BotService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -10,10 +15,14 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import static java.util.concurrent.Executors.newSingleThreadExecutor;
-
 @Configuration
 public class CommonBeans {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().enable(INDENT_OUTPUT);
+    }
 
     @Bean
     public BotSession botSession(BotService botService) throws TelegramApiException {
---
+++ compliment-bot/src/main/java/com/github/am_moving_lightspeed/compliment_bot/persistence/model/UserDao.java
@@ -0,0 +1,11 @@
+package com.github.am_moving_lightspeed.compliment_bot.persistence.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDao {
+
+    private Long id;
+}
