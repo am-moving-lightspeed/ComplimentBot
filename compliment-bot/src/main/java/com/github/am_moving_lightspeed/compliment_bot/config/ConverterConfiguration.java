dc33409631db95fe0f47ca27f5a00aa51008c46f
Aleh Ivoilau <aleh.ivoilau@resliv.by>
2024-06-23 00:29:54 +0300

    Broadcast refactoring

--- compliment-bot/src/main/java/com/github/am_moving_lightspeed/compliment_bot/config/ConverterConfiguration.java
+++ compliment-bot/src/main/java/com/github/am_moving_lightspeed/compliment_bot/config/ConverterConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class ConverterConfiguration {
 
     @Bean
