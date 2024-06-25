7e2a2401ceb8af7cb342ba0b69522b7066bcc0f5
Aleh Ivoilau <aleh.ivoilau@resliv.by>
2024-06-25 21:23:19 +0300

    Added dropbox integration

---
+++ compliment-bot/src/main/java/com/github/am_moving_lightspeed/compliment_bot/config/model/IntegrationClientProperties.java
@@ -0,0 +1,12 @@
+package com.github.am_moving_lightspeed.compliment_bot.config.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IntegrationClientProperties {
+
+    private String name;
+    private String accessToken;
+}
